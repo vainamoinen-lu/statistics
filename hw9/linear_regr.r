d40b542d404e9d2690f5e1fce6a1e5e580aa4f95
vainamoinen <weixinluwx@foxmail.com>
2018-12-03 03:30:43 +0800

    hw9

--- hw9/linear_regr.r
+++ hw9/linear_regr.r
@@ -12,13 +12,17 @@ linear_regression<- function()
 {
 y_x <- lm(y~x,xy_data)
 print(y_x)
+return(y_x)
 }
-linear_regression()
+yx_regr=linear_regression()
 
 scatter_plot<-function(plot_file)
 {
     png(file=plot_file)
-    plot(xy_data$x,xy_data$y,xlab='x',ylab='y',main='scatter point of (d)')
+    plot(xy_data$x,xy_data$y,xlab='x',ylab='y',main='scatter point')
+    abline(yx_regr)
+    abline(-1,0.5,lty=2,col='red')
+    legend('topright',legend=c('sample regression','population regression'),lty=c(1,2),col=c('black','red'))
     dev.off()
 }
 scatter_plot('scatter_plot_d.png')
